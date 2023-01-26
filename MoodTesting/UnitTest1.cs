ad16f76cd19d374ca3c17000a84a3b16ed543769
Amol <ajamnik.mech@gmail.com>
2023-01-26 23:19:28 +0530

    [Amol]- Any Mood Should Return Happy

--- MoodTesting/UnitTest1.cs
+++ MoodTesting/UnitTest1.cs
@@ -6,14 +6,24 @@ namespace MoodTesting
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GiveMoodShouldReturnSadMood()
         {
             const string happymood = "Happy";
             const string sadmood = "Sad";
-            MoodAnalser mood = new MoodAnalser(); //Arrange
-            string AcutalResult = mood.Mood(sadmood); //Act
+            MoodAnalser mood = new MoodAnalser();
+            string AcutalResult = mood.Mood(sadmood);
             string expected = "Sad";
-            Assert.AreEqual(expected, AcutalResult); //Assert
+            Assert.AreEqual(expected, AcutalResult);
+        }
+        [TestMethod]
+        public void GiveMoodShouldReturnHappMood()
+        {
+            const string happymood = "Happy";
+            const string sadmood = "Sad";
+            MoodAnalser mood = new MoodAnalser();
+            string AcutalResult = mood.Mood(happymood);
+            string expected = "Happy";
+            Assert.AreEqual(expected, AcutalResult);
         }
     }
 }
